97294e0c6697837af86c3f5a199eeb0bdd37ec62
Sai Karthik <saikarthiksistla@gmail.com>
2023-07-21 16:05:52 +0530

    logic and ui added

--- app/src/main/java/com/sri/counterappcompose/MainActivity.kt
+++ app/src/main/java/com/sri/counterappcompose/MainActivity.kt
@@ -3,13 +3,31 @@ package com.sri.counterappcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.sri.counterappcompose.ui.theme.CounterAppComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,30 +35,43 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             CounterAppComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                MyApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MyApp() {
+
+    var counter=remember{
+        mutableStateOf(0)
+    }
+    Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(counter.value.toString(), fontSize = 40.sp)
+            Spacer(Modifier.height(100.dp))
+            Card(
+                Modifier
+                    .size(145.dp)
+                    .clickable {
+                        counter.value++
+                    }, shape = CircleShape, elevation = CardDefaults.cardElevation(4.dp)
+            ) {
+                Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                    Text("TAP", color = Color.Magenta, modifier = Modifier, fontSize = 30.sp)
+                }
+            }
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
-fun GreetingPreview() {
+fun MyAppPreview() {
     CounterAppComposeTheme {
-        Greeting("Android")
+        MyApp()
     }
 }
